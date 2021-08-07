0ec6f210a02b6441343fd7cefa953a7119c8fff6
tualdev <2grulal2n@gmail.com>
2021-08-07 14:46:34 +0300

    update typing feature
    update typing feature

--- app/src/main/java/com/mvvm/githubapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/mvvm/githubapp/ui/main/MainActivity.kt
@@ -4,6 +4,10 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
@@ -29,6 +33,10 @@ class MainActivity : DataBindingActivity() {
   private lateinit var searchEditText: EditText
   private lateinit var clearButton: ImageButton
   private lateinit var filterImageButton: ImageButton
+
+  private var delay: Long = 1000
+  private var lastTextEdit: Long = 0
+  private var handler: Handler = Handler(Looper.getMainLooper())
 
   override fun onCreate(savedInstanceState: Bundle?) {
     onTransformationStartContainer()
@@ -64,43 +72,76 @@ class MainActivity : DataBindingActivity() {
       false
     })
 
+    searchEditText.addTextChangedListener(object : TextWatcher{
+
+      override fun afterTextChanged(s: Editable?) {
+
+        if (s!!.isNotEmpty()){
+          lastTextEdit = System.currentTimeMillis()
+          handler.postDelayed(inputFinishChecker, delay)
+        }
+      }
+
+      override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+      }
+
+      override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        handler.removeCallbacks(inputFinishChecker)
+      }
+
+    })
+
     filterImageButton.setOnClickListener {
 
       if (BuildConfig.FLAVOR == "free"){
         Toast.makeText(this, "it is free :(", Toast.LENGTH_SHORT).show()
       }else{
-        AlertDialog.Builder(this)
-                .setSingleChoiceItems(sortChoicesArray, 0, null)
-                .setPositiveButton(R.string.okay, DialogInterface.OnClickListener { dialog, _ ->
-                  dialog.dismiss()
-                  val selectedPosition: Int = (dialog as AlertDialog).listView.checkedItemPosition
-
-                  val query: String = searchEditText.text.toString().trim { it <= ' ' }
-                  if (query != "") {
-
-                    when (selectedPosition) {
-                      0 -> {
-                        viewModel.fetchRepoList(query, "stars", 0)
-                        searchEditText.clearFocus()
-                      }
-                      1 -> {
-                        viewModel.fetchRepoList(query, "forks", 0)
-                        searchEditText.clearFocus()
-                      }
-                      2 -> {
-                        viewModel.fetchRepoList(query, "updated", 0)
-                        searchEditText.clearFocus()
-                      }
-                    }
-
-                  } else {
-                    dialog.dismiss()
-                    Toast.makeText(this, getString(R.string.please_enter_input), Toast.LENGTH_SHORT).show()
-                  }
-
-                }).show()
+        showSortDialog(sortChoicesArray)
       }
     }
+  }
+
+  private val inputFinishChecker = Runnable {
+    if (System.currentTimeMillis() > lastTextEdit + delay - 500) {
+      val query: String = searchEditText.text.toString().trim { it <= ' ' }
+      if (query != "") {
+        viewModel.fetchRepoList(query, "best%20match", 0)
+        searchEditText.clearFocus()
+      }
+    }
+  }
+
+  private fun showSortDialog(sortChoicesArray: Array<CharSequence>) {
+    AlertDialog.Builder(this)
+            .setSingleChoiceItems(sortChoicesArray, 0, null)
+            .setPositiveButton(R.string.okay, DialogInterface.OnClickListener { dialog, _ ->
+              dialog.dismiss()
+              val selectedPosition: Int = (dialog as AlertDialog).listView.checkedItemPosition
+
+              val query: String = searchEditText.text.toString().trim { it <= ' ' }
+              if (query != "") {
+
+                when (selectedPosition) {
+                  0 -> {
+                    viewModel.fetchRepoList(query, "stars", 0)
+                    searchEditText.clearFocus()
+                  }
+                  1 -> {
+                    viewModel.fetchRepoList(query, "forks", 0)
+                    searchEditText.clearFocus()
+                  }
+                  2 -> {
+                    viewModel.fetchRepoList(query, "updated", 0)
+                    searchEditText.clearFocus()
+                  }
+                }
+
+              } else {
+                dialog.dismiss()
+                Toast.makeText(this, getString(R.string.please_enter_input), Toast.LENGTH_SHORT).show()
+              }
+
+            }).show()
   }
 
   private fun searchAction() {
